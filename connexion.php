8553a86b17c646381343cf10a24082fe37eee14a
TimmyKham <tkhamsithideth@gmail.com>
2019-01-02 10:56:04 +0100

    Added php mysql blog

---
+++ connexion.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+</html>
+<html>
+   <head>
+      <title>Connexion</title>
+      <meta charset="utf-8">
+      <link rel="stylesheet" type="text/css" href="css/connexion.css">
+   </head>
+   <body>
+      <div id=container>
+         <form method="POST" action="">
+          <h1>Connexion</h1>
+            <label><b>E-mail</b></label>
+            <input type="email" name="Email" placeholder="votre@email.fr"" />
+            <label><b>Mot de Passe</b></label>
+            <input type="password" name="MotDePasse" placeholder="********" />
+            <input type="submit" name="submit" value="Se connecter !" />
+         </form>
+      </div>
+   </body>
+</html>
---
+++ newblogtimmy.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="css/newblogtimmy.css">
+</head>
+<body src="/images/blur-background.jpg">
+
+<div class="header">
+  <h2>Blog de Timmy</h2>
+</div>
+
+<div class="row">
+  <nav class="card">
+    <ul>
+     <li><a class="nav" href="#">Accueil</a></li>
+     <li><a class="nav" href="createaccount.php">Crée un compte</a></li>
+     <li><a class="nav" href="connexion.php">Connexion</a></li>
+     <li><a class="nav" href="profil.php">Profil</a></li>
+    </ul>
+  </nav>
+  <div class="leftcolumn">
+    <div class="card">
+        <h2>SUPER SMASH BROS ULTIMATE</h2>
+        <h5>Timmy Khamsithideth, Dec 27, 2018</h5>
+        <div><img class="fakeimg" style="height:200px;" src="images/bannersmash.jpg""></div>
+        <p class="trailer"><strong>"LE JEU DE L'ANNÉE !"</strong></p>
+        <p>Super Smash Bros. Ultimate (大乱闘スマッシュブラザーズ SPECIAL, Dairantō Sumasshu Burazāzu Supesharu?) est un jeu vidéo de combat et de plates-formes développé par Sora Ltd. et Bandai Namco et édité par Nintendo. Il s'agit du cinquième épisode de la série Super Smash Bros.. Annoncé lors du Nintendo Direct du 8 mars 2018, il est sorti mondialement le 7 décembre 2018 exclusivement sur Nintendo Switch.</p>
+    </div>
+    <div class="card">
+        <h2>LEAGUE OF LEGENDS</h2>
+        <h5>Timmy Khamsithideth, Dec 27, 2018</h5>
+        <div><img class="fakeimg" style="height:200px;" src="images/bannerlol.jpg""></div>
+        <p class="trailer"><strong>"UN VÉRITABLE PHÈNOMÈNE !"</strong></p>
+        <p>League of Legends (abrégé LoL), anciennement nommé League of Legends: Clash of Fates est un jeu vidéo de type arène de bataille en ligne (MOBA) gratuit développé et édité par Riot Games sur Windows et Mac OS X.
+        C'est en 2013 qu'il devient l'un des jeux les plus joués au monde. Il séduit aussi le monde du sport électronique en devenant l'événement eSport le plus regardé de l'histoire, avec plus de 32 millions de spectateurs en ligne et plus de 8,5 millions de vues simultanées lors de la finale de la saison 3 du championnat du monde en 2013</p>
+    </div>
+  </div>
+  <div class="rightcolumn">
+    <div class="card">
+      <h2>Utilisateur</h2>
+    <div class="comment" style="height:100px;">
+    </div>
+      <p>Bonjour</p>
+    </div>
+
+    <div class="card">
+      <h3>Commentaires</h3>
+      <div class="comment">
+        <p>"WOW"</p>
+        <p>Timmy.K</p>
+      </div><br>
+      <div class="comment">
+      <p>"Excellent"</p>
+      <p>Timmy.K</p>
+    </div><br>
+      <div class="comment">
+      <p>"Génial"</p>
+      <p>Timmy.K</p></div>
+    </div>
+    <div class="card">
+      <h3>INFOS</h3>
+      <p>blabla</p>
+    </div>
+  </div>
+</div>
+
+<div class="footer">
+  <p>Poster par : Timmy Khamsithideth</p>
+  <p>Contact: <a href="mailto:tkhamsithideth@gmail.com">
+  tkhamsithideth@gmail.com</a>.</p>
+</div>
+
+</body>
+</html>
