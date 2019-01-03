6d456bc3d9fa62c4994ac29883075af289e2ce0f
TimmyKham <tkhamsithideth@gmail.com>
2019-01-03 23:17:23 +0100

    Admin interface page php

---
+++ menuadmin.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" >
+   <head>
+     <title>Profil</title>
+        <link rel="stylesheet" type="text/css" href="css/menuadmin.css" />
+       <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+   </head>
+   <body>
+    <div id="container"> 
+      <h1>Session Administrateur</h1>
+        <div class="big-cube">
+            <button class="cube" onclick="location.href='profil.php'"><p>Profil</p></button>
+            <button class="cube" onclick="location.href='profil.php'"><p>Admin</p></button>
+            <button class="cube" onclick="location.href='#'"><p>Article</p></button>
+            <button class="cube" onclick="location.href='#'"><p>Commentaire</p></button>
+            <button class="cube" onclick="location.href='gestion.php'"><p>Utilisateurs</p></button>
+            <button class="cube" onclick="location.href='#'"><p>Ajouter</p></button>
+            <button class="cube" onclick="location.href='#'"><p>Modifier</p></button>
+            <button class="cube" onclick="location.href='#'"><p>Supprimer</p></button>
+        </div> 
+    </div>
+  </body>
+  
+</html>
