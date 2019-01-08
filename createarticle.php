b2f5dae3d1e9ca088a239bfba1b17b903506a65d
TimmyKham <tkhamsithideth@gmail.com>
2019-01-08 12:25:30 +0100

    Adding article creator file

---
+++ createarticle.php
@@ -0,0 +1,48 @@
+<?php
+session_start ();
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" >
+   <head>
+     <title>Création article</title>
+        <link rel="stylesheet" type="text/css" href="css/createarticle.css" />
+       <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+   </head>
+   <body>
+    <div id="container">  
+      <form method="post">
+        <h1>Création d'un article</h1>
+        <label><b>Titre</b></label>
+        <input type="text" placeholder="Entrer le titre" name="titre" required>
+        <label><b>Date de l'article</b></label>
+        <input type="date" name="date_article" required>
+        <label><b>Titre aguicheur</b></label>
+        <input type="text" placeholder="Entrer un titre aguicheur" name="gros_titre" required>
+        <label><b>Description</b></label>
+        <input type="text" placeholder="Description de l'article" name="description" required>
+        <input type="submit" name="submit" value="Création de l'article">
+      </form>
+    </div>
+  </body>
+  
+  <?php
+    try {
+        $bdd = new PDO('mysql:host=localhost;dbname=blogtimmy', 'root', 'root'); 
+
+        if(isset($_POST['submit'])){
+          $titre=$_POST['titre'];
+          $date_article=$_POST['date_article'];
+          $gros_titre=$_POST['gros_titre'];
+          $description=$_POST['description'];
+
+          $bdd->exec("INSERT INTO Article (titre, id_compte, date_article, gros_titre, description) 
+                VALUES('$titre','12','$date_article','$gros_titre','$description')");
+          header('Location: newblogtimmy.php');
+          exit();
+        }
+    } catch (PDOException $e) {
+        echo 'Connexion échouée : ' . $e->getMessage();
+    }
+    
+  ?>
+</html>
--- newblogtimmy.php
+++ newblogtimmy.php
@@ -21,19 +21,25 @@
   </nav>
   <div class="leftcolumn">
     <div class="card">
-        <h2>SUPER SMASH BROS ULTIMATE</h2>
-        <h5>Timmy Khamsithideth, Dec 27, 2018</h5>
-        <div><img class="fakeimg" style="height:200px;" src="images/bannersmash.jpg""></div>
-        <p class="trailer"><strong>"LE JEU DE L'ANNÉE !"</strong></p>
-        <p>Super Smash Bros. Ultimate (大乱闘スマッシュブラザーズ SPECIAL, Dairantō Sumasshu Burazāzu Supesharu?) est un jeu vidéo de combat et de plates-formes développé par Sora Ltd. et Bandai Namco et édité par Nintendo. Il s'agit du cinquième épisode de la série Super Smash Bros.. Annoncé lors du Nintendo Direct du 8 mars 2018, il est sorti mondialement le 7 décembre 2018 exclusivement sur Nintendo Switch.</p>
-    </div>
-    <div class="card">
-        <h2>LEAGUE OF LEGENDS</h2>
-        <h5>Timmy Khamsithideth, Dec 27, 2018</h5>
-        <div><img class="fakeimg" style="height:200px;" src="images/bannerlol.jpg""></div>
-        <p class="trailer"><strong>"UN VÉRITABLE PHÈNOMÈNE !"</strong></p>
-        <p>League of Legends (abrégé LoL), anciennement nommé League of Legends: Clash of Fates est un jeu vidéo de type arène de bataille en ligne (MOBA) gratuit développé et édité par Riot Games sur Windows et Mac OS X.
-        C'est en 2013 qu'il devient l'un des jeux les plus joués au monde. Il séduit aussi le monde du sport électronique en devenant l'événement eSport le plus regardé de l'histoire, avec plus de 32 millions de spectateurs en ligne et plus de 8,5 millions de vues simultanées lors de la finale de la saison 3 du championnat du monde en 2013</p>
+        <?php
+    try {
+        $bdd = new PDO('mysql:host=localhost;dbname=blogtimmy;charset=utf8', 'root', 'root');
+    }
+    catch(Exception $e) {
+        die('Erreur : '.$e->getMessage());
+    }
+    $reponse = $bdd->query('SELECT * FROM Account INNER JOIN Article ON Account.id = Article.id_compte ORDER BY id_compte DESC');
+    while ($donnees = $reponse->fetch())  {
+    ?>
+            <h2><?php echo $donnees['titre']; ?></h2>
+            <h5><?php echo $donnees['Prenom']; ?> <?php echo $donnees['Nom']; ?>, <?php echo $donnees['date_article']; ?></h5> 
+            <div><img class="fakeimg" style="height:200px;" src="images/bannersmash.jpg""></div>
+            <p class="trailer"><strong><?php echo $donnees['gros_titre']; ?></strong></p>
+            <p><?php echo $donnees['description']; ?></p>
+    <?php
+    }
+    $reponse->closeCursor();
+    ?>
     </div>
   </div>
   <div class="rightcolumn">
